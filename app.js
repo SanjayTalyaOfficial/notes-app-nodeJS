978487ab62e023caf342b70378a72a70d1643a50
SanjayTalya <sanjaytj@gmail.com>
2020-07-19 14:08:27 +0530

    chalk package

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-// const add = require('./utils');  //we are exporting the name from utils.js
-// const sum = add(3,4);
-// console.log(sum);
+// // const add = require('./utils');  //we are exporting the name from utils.js
+// // const sum = add(3,4);
+// // console.log(sum);//
+// const validator = require('validator')
+// let getNotes = require('./notes');
+// const note = getNotes('morning this is my first msg');
+// console.log(note);
+// console.log(validator.isEmail('sanjaytj@gmail.com'))
+// console.log(validator.isURL('sanjaytalya.com'))
 
-let getNotes = require('./notes');
-const note = getNotes('morning this is my first msg');
-console.log(note);
+const chalk = require('chalk')
+console.log(chalk.black.bgGreen('Success!'));

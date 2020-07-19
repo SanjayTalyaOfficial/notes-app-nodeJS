02c07dcf32348f6612158f4057a1bef172e17874
SanjayTalya <sanjaytj@gmail.com>
2020-07-20 01:11:12 +0530

    yargs commands

--- app.js
+++ app.js
@@ -1,12 +1,61 @@
 
 let getNotes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-const command = process.argv[2];
+// customise yargs version 
+yargs.version('1.1.0')
 
-if (command === 'add') {
-    console.log('Adding note!');
-} else if(command === 'remove')
-{
-    console.log('Removing note!')
-}
+// we want the app to add, remove, read and list
+
+// create a add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note description',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        console.log('Title: ' +argv.title)
+        console.log('Description: '+argv.body)
+    }
+})
+
+//create a remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+})
+
+// Create a list command
+yargs.command({
+    command: 'list',
+    describe: 'Show the list of notes',
+    handler: function () {
+        console.log('There are the list of functions')
+    }
+})
+
+// Create a read command
+yargs.command({
+    command: 'read',
+    describe: 'read the note',
+    handler: function () {
+        console.log('Reading the notes')
+    }
+})
+yargs.parse()
+
+// console.log(yargs.argv)
